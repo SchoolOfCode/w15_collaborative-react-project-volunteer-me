1fca3677c8e7ba7137f4f9d268c87505e66b163e
katehrsn <kate.hrsn@gmail.com>
2021-07-07 17:15:43 +0100

    rendered button and tested css

---
+++ src/components/listen-button/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.css'
+
+function ListenButton (){
+    return (
+        <button className="button"> Listen to this page </button>
+    )
+}
+
+export default ListenButton
